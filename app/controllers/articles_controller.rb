27d62f55a4b265ea0dffee1918344f184cbbab53
Tharsikan <thar.navaratnam@gmail.com>
2022-01-04 18:15:37 +0000

    add basic authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "abcdef", password: "secret", except: [:index, :show]
+  
   def index
     @articles = Article.all
   end
