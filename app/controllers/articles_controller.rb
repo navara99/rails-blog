84145a73d7fcd51ed7128aca2527de69ee3ed5ba
Tharsikan <thar.navaratnam@gmail.com>
2022-01-04 17:26:32 +0000

    add update action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,16 @@ class ArticlesController < ApplicationController
 
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def article_params
